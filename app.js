3b0aa2c45dba7e6c36237ced61cda471397ceb90
ink <eric.zmo2@gmail.com>
2020-03-14 21:56:53 -0700

    working app

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ const Engineer = require("./lib/Engineer")
 const Intern = require("./lib/Intern")
 
 const OUTPUT_DIR = path.resolve(__dirname,"output")
-
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
-// const render = require("./lib/htmlRenderer");
-
 const outputPath = path.join(OUTPUT_DIR,"team.html")
 const render = require("./lib/htmlRenderer")
 let inpArray = [];
@@ -84,6 +80,8 @@ function getInfo(){
             }
             console.log(inpArray)
             keepAdd(res)
+            let teamHtml = render(inpArray)
+            fs.writeFileSync(outputPath,teamHtml)
         })
 }
 
@@ -94,8 +92,7 @@ function keepAdd(res){
         console.log("You have added all team members")
     }
 }
+
 getInfo()
 
-inpArray.forEach(e => {
-    render(e)
-});
+
